a4485f41dedbd124a8eeb01030cdbed2ac1d3f5d
alinaPopad <popadchenko.a@gmail.com>
2023-08-24 10:32:09 +0000

    Ревью 1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,19 @@
 import os
 from pathlib import Path
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['84.201.154.246','127.0.0.1', 'localhost', 'kittygram0823.ddns.net']
+ALLOWED_HOSTS = [
+    '84.201.154.246',
+    '127.0.0.1',
+    'localhost',
+    'kittygram0823.ddns.net'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
